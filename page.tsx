eca7b7b673400d5833859f97e4453232d233ebe6
hammys20 <hammys.trading@gmail.com>
2026-01-29 19:43:05 -0500

    test

--- page.tsx
+++ page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import WhatnotStatus from "@app/components/WhatnotStatus";
+import WhatnotStatus from "./components/WhatnotStatus";
 
 export default function HomePage() {
   return (
